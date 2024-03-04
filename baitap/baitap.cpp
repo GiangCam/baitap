57384500dad7fbe3af623b251c330dbb996fe4a7
ADMIN <ADMIN@LAB1-MAY10>
2024-03-04 11:19:04 +0700

    Add project files.

---
+++ baitap/baitap.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int so, tongle = 0;
+	cout << "nhap so";
+	cin >> so;
+	for (int i = 0; i <= so; i++) {
+		if (1 % 2 != 0) // số lẽ là số chia 2 dư 1
+		{
+			cout << i << " ";
+			tongle = tongle + i;
+		}
+	}
+	cout << "\n tong cac so le o don " << so << "la " << tongle;
+	return 0;
+}
